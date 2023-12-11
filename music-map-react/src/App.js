965b33d5d5f9dbd7ad95b0430a5107dc21f1661c
jedrzej <j.klamra@gmail.com>
2023-12-11 16:08:52 +0100

    updated the view, made it better ! :)

--- music-map-react/src/App.js
+++ music-map-react/src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HamburgerMenu from './HamburgerMenu';
 
 import './App.css';
 
 const App = () => {
+
+  const [showSignOut, setShowSignOut] = useState(false);
+
   return (
-    <div className="header-container">
-      <HamburgerMenu />
-      <div className="header-text">Music-Map loool xdd!</div>
-      <div className="header-text">no i essa xdd!</div>
+    <div>
+      <div className="header-container">
+        <div className="left-section">
+          <HamburgerMenu />
+          <div className="header-text">Music-Map</div>
+        </div>
+        <div className="sign-in-container" onMouseEnter={() => setShowSignOut(true)} onMouseLeave={() => setShowSignOut(false)}>
+          <div className="header-text">Logged in as X</div>
+          <div className={`sign-out-option ${showSignOut ? 'show' : ''}`} onClick={() => console.log('Sign out clicked')}>
+            Sign out
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
